7db0d663fcae8aab7276e41cb614c7acbaf38bb8
Hanchen Xing <hanchen.xing@outlook.com>
2024-07-28 00:36:37 +0200

    Added password protection page and updated paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,28 +1,29 @@
-const markdownIt = require("markdown-it");
-const md = new markdownIt({
-  html: true,
-});
-
-const createCollectionsAndFilters = require("./_utils/index.js");
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addFilter("markdown", (content) => {
-    if (typeof content === "string") {
+  const markdownIt = require('markdown-it');
+  const md = new markdownIt({
+    html: true,
+  });
+
+  const createCollectionsAndFilters = require('./_utils/index.js');
+
+  eleventyConfig.addFilter('markdown', (content) => {
+    if (typeof content === 'string') {
       return md.render(content);
     }
     return content;
   });
 
-  eleventyConfig.addPassthroughCopy({ "theme/assets": "assets" });
-  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy({ 'theme/assets': 'assets' });
+  eleventyConfig.addPassthroughCopy('admin');
+  eleventyConfig.addPassthroughCopy('password.html');
 
   createCollectionsAndFilters(eleventyConfig);
 
   return {
     dir: {
-      input: "cms",
-      includes: "../theme",
-      output: "public",
+      input: 'cms',
+      includes: '../theme',
+      output: 'public',
     },
   };
 };
